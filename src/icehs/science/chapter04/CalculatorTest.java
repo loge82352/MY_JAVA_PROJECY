c71d559085217c7cefe63094bd9471d08ec6d380
User <User@DESKTOP-0V5COK6>
2020-12-30 20:32:07 +0900

    Chapter04

---
+++ src/icehs/science/chapter04/CalculatorTest.java
@@ -0,0 +1,22 @@
+package icehs.science.chapter04;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		
+		int number1 = 20;
+		int number2 = 7;
+		
+		System.out.println("µ¡¼À°á°ú");
+		System.out.println("»¬¼À°á°ú");
+		System.out.println("°ö¼À°á°ú");
+		System.out.println("³ª´©±â°á°ú");
+		System.out.println("³ª¸ÓÁö°á°ú");
+		
+		
+		
+		
+		
+	}
+
+}
