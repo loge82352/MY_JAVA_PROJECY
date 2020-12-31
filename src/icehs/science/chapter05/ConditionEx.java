e0a926898cfed30e4cfc10ab32f74783cc0b029f
User <User@DESKTOP-0V5COK6>
2020-12-31 20:51:39 +0900

    Chapter04

---
+++ src/icehs/science/chapter05/ConditionEx.java
@@ -0,0 +1,26 @@
+package icehs.science.chapter05;
+
+public class ConditionEx {
+
+	public static void main(String[] args) {
+	
+		int age = 20;
+		if (age <19) {
+			System.out.println("19세 이상만 입장 가능합니다.");
+			System.out.println("죄송합니다.");
+		}
+		if (age >= 19) {
+			
+			System.out.println("성입입니다.");
+			System.out.println("입장하세요.");
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
